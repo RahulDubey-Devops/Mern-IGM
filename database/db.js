e61100f9b9c97808071f5d8e431428bb6c6045cf
RahulDubey-Devops <rdubey@metadrob.com>
2024-10-09 15:44:32 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI; // Make sure this matches your env variable name
+const uri = process.env.MONGODB_URL; // Make sure this matches your env variable name
 
 const connectToMongo = async () => {
     try {
