e2b4595f0aebab180370f8cf68cae14b2f4dfd78
RahulDubey-Devops <rdubey@metadrob.com>
2024-10-09 17:13:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.use('/api/quotations', quotationRoutes);
 
 connectToMongo();
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+    // console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+    console.log("App is Listening on Port 5000");
 })
